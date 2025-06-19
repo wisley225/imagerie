c40013d36f639b8c8a22971a62927bd22d6afd79
LEWIS GUEI <lewisguei67@gmail.com>
2025-06-19 03:56:40 +0000

    new fond-end

--- app/accueil/header.tsx
+++ app/accueil/header.tsx
@@ -1,28 +1,49 @@
 'use client'
 import React, { useState, useRef } from 'react';
 import Image from 'next/image';
-
+import '../../app/globals.css'
 const Header = () => {
  
 
   return (
-    <div className=''>
-      {/* <div className='border h-60 place-content-center place-items-center bg-black text-white'>
-        <h1 className='text-5xl font-black mb-2 font-serif'>Votre éditeur de photo en ligne</h1>
-        <h2 className='font-serif'>Une multitude d'outils gratuits pour éditer vos images par lot</h2>
-      </div> */}
+    <div className=' px-5   bg-gradient-to-tl   from-cyan-100 via-teal-100 h-svh  pt-20'>
 
-<div>
-  <div className=' relative h-80 w-40'>
- <Image
- src='/imgHead.png'
- fill
- alt='header image'
-
- />
-  </div>
-
+<div className=' absolute  top-72 z-20  mb-5    w-5/12 flex flex-col  items-center'>
+<h1 className=' mb-5   text-center  text-4xl uppercase font-black font-serif '>Le traitement d’images simple, rapide et puissant. </h1>
+<p className=' mb-5  '>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Enim, temporibus odit sunt numquam modi facere earum corrupti quos? Repellendus quam delectus amet cum voluptatum, fuga similique eaque earum, dolore asperiores dolor quidem qui reprehenderit nesciunt reiciendis ea, tempore non eius.</p>
+<button className=' cursor-pointer hover:bg-cyan-500 transition-all border bg-cyan-600 text-white text-xl px-4 py-1 rounded-full'>Get Started</button>
 </div>
+
+
+
+   <div className=' paysage relative left-[600px]  h-60 w-80  -rotate-10   rounded-md shadow-2xl '>
+ <Image
+  src='/paysage1.jpg'
+  alt='paysage'
+ fill
+ className=' object-cover border rounded-md'
+  />
+    </div>   
+ 
+  <div className=' paysage relative h-60 w-80 left-[900px] bottom-40 rotate-10 rounded-md shadow-2xl '>
+ <Image
+  src='/paysage2.jpg'
+  alt='paysage'
+ fill
+ className=' object-cover border rounded-md'
+  />
+    </div> 
+      <div className=' paysage relative h-60 w-80  left-[700px] bottom-60 rotate-5 rounded-md shadow-2xl '>
+ <Image
+  src='/paysage3.jpg'
+  alt='paysage'
+  fill
+ className=' object-cover border rounded-md'
+  />
+    </div> 
+
+
+  
    
     </div>
   );
